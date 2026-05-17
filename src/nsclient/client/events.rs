79793be077df61ddf6a9eb29117facbb40bad887
Michael Medin <michael@medin.name>
2026-05-17 10:30:24 +0200

    Moved from nscp repo

---
+++ src/nsclient/client/events.rs
@@ -0,0 +1,23 @@
+use crate::nsclient::client::command_input::Command;
+use crate::nsclient::client::log_widget::LogRecord;
+use crossterm::event::KeyEvent;
+use tokio::sync::mpsc;
+
+pub async fn send_or_error<T>(sender: &mpsc::Sender<T>, event: T) {
+    if let Err(e) = sender.send(event).await {
+        eprintln!("Error sending event: {}", e);
+    }
+}
+pub enum UIEvent {
+    Key(KeyEvent),
+    Status(String),
+    Output(String),
+    Error(String),
+    Performance(f64, f64, f64),
+    Log(LogRecord),
+    Commands(Vec<String>),
+}
+
+pub enum UICommand {
+    Command(Command),
+}
